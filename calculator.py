f48d0dd32e9361dff78f2200b1d7dbe9025f19f7
momokowv <161792955+momokowv@users.noreply.github.com>
2024-05-10 14:35:56 +0900

    Update calculator.py
    Co-authored-by: yohei-ozao <161719400+yohei-ozao@users.noreply.github.com>

--- calculator.py
+++ calculator.py
@@ -66,7 +66,24 @@ def exp():
     else:
         print("入力値が正しくありません")
 
-def calculate_twice():
+def calculate_twice(result):
+    sub_choice = input("演算項目を選択してください(1-4): ")
+    pre_result = result
+    if sub_choice in ["1", "2", "3", "4"]:
+        num = input("演算を行う数値を入力してください: ")
+        if is_numeric(num):
+            if sub_choice == "1":
+                result += float(num)
+            elif sub_choice == "2":
+                result -= float(num)
+            elif sub_choice == "3":
+                result *= float(num)
+            elif sub_choice == "4":
+                result /= float(num)
+            print(f"計算結果 : 直前の結果 {pre_result} に対する演算 {sub_choice} を行った結果 = {result}")
+            return result
+        else:
+            print("入力値が正しくありません")
     """
     ユーザーに処理したい演算と値を入力してもらい、直前の演算結果に対する演算結果を返す（演算は、加算・減算・乗算・除算の中から選択）
     """
